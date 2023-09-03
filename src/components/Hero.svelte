d10c3d0bf8618a489ee01b8a90f74cc0368a0872
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-09-03 15:46:59 +0800

    add IntervalCard and interval time calculation

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-    let trainingTime: string;
-    let intervals: string;
+    import IntervalCard from "./IntervalCard.svelte";
+
+    let trainingTimeBuff: string;
+    let intervalsBuff: string;
+
+    let trainingTime: number;
+    let intervals: number;
 
     const debug = () => {
-        console.log(trainingTime);
-        console.log(intervals);
+        trainingTime = Number(trainingTimeBuff);
+        intervals = Number(intervalsBuff);
     }
+
+    $: intervalTime = trainingTime / intervals;
 </script>
 
 <main>
@@ -14,10 +21,14 @@
             Welcome to your Training Buddy!
         </h1>
         <p class="text-center pt-10 text-2xl">
-            I want to train for <span bind:innerText={trainingTime} contenteditable>30</span> minutes in <span bind:innerText={intervals}
+            I want to train for <span bind:innerText={trainingTimeBuff} contenteditable>30</span> minutes in <span bind:innerText={intervalsBuff}
             contenteditable>5</span> minute intervals
         </p>      
 
+        {#each {length: intervals} as _, interval} 
+            <IntervalCard time={intervalTime}/>
+        {/each}
+        
         <button class="absolute inset-x-0 bottom-0 align-center h-12 px-8 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg 
                     focus:shadow-outline hover:bg-indigo-800" 
                     on:click={debug}>Start</button>
---
+++ src/components/IntervalCard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let time: number;
+</script>
+
+<main>
+    <div>
+        <p class="text-red-500">{time} minutes</p>
+    </div>
+</main>
