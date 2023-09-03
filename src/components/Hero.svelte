5ce7652a2271e267433b5194c24905c43b6ba5ce
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-09-03 12:32:33 +0800

    first commit

---
+++ src/components/Hero.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let trainingTime: string;
+    let intervals: string;
+
+    const debug = () => {
+        console.log(trainingTime);
+        console.log(intervals);
+    }
+</script>
+
+<main>
+    <div>
+        <h1 class="text-center pt-10 text-6xl font-bold">
+            Welcome to your Training Buddy!
+        </h1>
+        <p class="text-center pt-10 text-2xl">
+            I want to train for <span bind:innerText={trainingTime} contenteditable>30</span> minutes in <span bind:innerText={intervals}
+            contenteditable>5</span> minute intervals
+        </p>      
+
+        <button on:click={debug}>Click me</button>
+    </div>    
+</main>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import '../app.css'
+</script>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.slate.800);
+    }
+</style>
+
+<body class="text-white">
+    <slot />
+</body>
