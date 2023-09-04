109ecf06d7810101539ebd96ad2577d9c18ddcce
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-09-04 20:44:26 +0800

    add complete button

--- src/components/IntervalCard.svelte
+++ src/components/IntervalCard.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
     export let time: number;
+
+    let cardComplete: boolean;
+    $: cardStyle = `card w-96 bg-primary text-primary-content ${cardComplete ? "line-through" : " "}`
+
+    let completeCard = () => {
+        cardComplete = !cardComplete;
+    }
 </script>
 
 <main>
     <div>
         <br />
-        <div class="card w-96 bg-primary text-primary-content">
+        <div class={cardStyle}>
             <div class="card-body">
               <h2 class="card-title">{time} minutes</h2>
               <p>placeholder</p>
+              <div class="card-actions justify-end">
+                <button on:click={completeCard} class="btn">Done</button>
+              </div>
             </div>
         </div>
-
         <br />
     </div>
 </main>
